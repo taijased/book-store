5bc2dcff88084d05d487e9d5a95ebc2d3220233b
Spiridonov Maxim <taijased@gmail.com>
2020-02-06 16:47:17 +0300

    Initial Commit

---
+++ book-store/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  book-store
+//
+//  Created by Максим Спиридонов on 06.02.2020.
+//  Copyright © 2020 Максим Спиридонов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
